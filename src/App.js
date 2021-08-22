aac08ab8591eae9e556d2cf6fa26f582ecfce04f
VengerAlex <alexvenger90@gmail.com>
2021-08-23 00:46:33 +1200

    got users from github/API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-const App = (props) => {
-  return (
-      <h1>hello</h1>
-  )
+import {Component} from "react";
+import axios from "axios";
+
+import Navbar from "./components/layouts/Navbar";
+import Users from "./components/users/Users";
+
+
+class App extends Component {
+    state = {
+        users: [],
+        loading: false
+    }
+
+    async componentDidMount() {
+        this.setState({loading: true});
+        console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET)
+        console.log('+')
+        const res = await axios.get(`https://api.github.com/users?client_id=$
+        {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+        {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+        this.setState({users: res.data, loading: false});
+    }
+
+    render() {
+        return (
+            <div className='app'>
+                <Navbar/>
+                <div className="container">
+                    <Users
+                        loading={this.state.loading}
+                        users={this.state.users}
+                    />
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
+
